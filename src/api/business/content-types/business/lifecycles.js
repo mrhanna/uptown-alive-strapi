6172869b0f56e6bf0602927604efc2d03eef4f0f
mrhanna <hanna.michaelr@gmail.com>
2024-01-31 17:52:09 -0600

    Cleaned up lifecycles

--- src/api/business/content-types/business/lifecycles.js
+++ src/api/business/content-types/business/lifecycles.js
@@ -3,11 +3,8 @@ const beforeSave = async function (event) {
 
     if (data.address && data.coordinate === null) {    
         try {
-            console.log(`https://nominatim.openstreetmap.org/search?q=${data.address}&format=jsonv2`)
             const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${data.address}&format=jsonv2`);
-
             const coord = await response.json();
-            console.log(coord);
 
             const tempdata = await strapi.query('general.coordinate').create({ 
                 data: {
@@ -15,7 +12,6 @@ const beforeSave = async function (event) {
                     longitude: coord[0].lon,
                 }
             });
-            console.log(tempdata);
 
             data.coordinate = {
                 id: tempdata.id,
